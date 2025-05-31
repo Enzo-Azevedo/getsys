03ce728b80f0eadf03a11d5f6332a54059ca3246
Enzo Azevedo <enzoteste3.g@gmail.com>
2025-05-31 08:07:01 -0300

    add gputil and psutil

--- app/main.py
+++ app/main.py
@@ -1,6 +1,10 @@
 import requests as req
 from secrets import token_urlsafe, SystemRandom
+import platform
+import gputil
 
-user_key = token_urlsafe(SystemRandom().getrandbits(9))
-print(user_key)
+user_key = token_urlsafe(SystemRandom().getrandbits(7))
+
+system_info = platform.uname()
+print(system_info.processor)
 
