e05fe54c3007946caad7dcb7b790854d94b09598
Enzo Azevedo <enzoteste3.g@gmail.com>
2025-05-31 08:08:31 -0300

    backing pipfile

--- app/main.py
+++ app/main.py
@@ -1,10 +1,14 @@
 import requests as req
 from secrets import token_urlsafe, SystemRandom
 import platform
-import gputil
+import GPUtil
+import psutil
 
 user_key = token_urlsafe(SystemRandom().getrandbits(7))
 
 system_info = platform.uname()
 print(system_info.processor)
 
+user_data = {
+    
+}
