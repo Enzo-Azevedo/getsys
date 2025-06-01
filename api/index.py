c3dabc99aa7f1b0e21a4e59cf65e6b7e25a9856d
Enzo Azevedo <enzoteste3.g@gmail.com>
2025-06-01 18:20:06 -0300

    changed api method request for test

--- api/index.py
+++ api/index.py
@@ -2,6 +2,7 @@ from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
-@app.route("/", methods=["GET"])
-def home():
-    return jsonify({"message": "API funcionando!"})
+@app.route("/dados", methods=["POST"])
+def receber_dados():
+    dados = request.json  # ou request.form se for form-data
+    return jsonify({"recebido": dados}), 200
