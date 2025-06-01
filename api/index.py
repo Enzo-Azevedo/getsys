4363ee2f9048d3e13405728aedbeb98562e65abf
Enzo Azevedo <enzoteste3.g@gmail.com>
2025-06-01 18:25:39 -0300

    a

--- api/index.py
+++ api/index.py
@@ -2,7 +2,6 @@ from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
-@app.route("/", methods=["POST"])
-def receber_dados():
-    dados = request.json  # ou request.form se for form-data
-    return jsonify({"recebido": dados}), 200
+@app.route("/", methods=["GET"])
+def home():
+    return jsonify({"message": "API funcionando!"})
